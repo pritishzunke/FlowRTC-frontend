a6955115737e61bb11d36f266c0ab5e22167b8c1
Pritish <pritish@Pritishs-MacBook-Air.local>
2024-12-06 17:35:52 +0530

    removed bloadware

--- flowrtc/src/App.js
+++ flowrtc/src/App.js
@@ -1,19 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { io } from "socket.io-client";
-import Landing from "./pages/landing";
+// import React, { useEffect, useState } from "react";
+// import { io } from "socket.io-client";
+// import Landing from "./pages/landing";
 
+import Home from "./pages/home";
+
+// import Home from "./pages/home";
 const App = () => {
-  const [socket, setSocket] = useState();
-  // Connect to the Socket.IO server
-  useEffect(() => {
-    const socket = io("http://localhost:8080"); // Replace with your server URL
-    // Clean up on component unmount
-    setSocket(socket);
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
-  return <Landing socket={socket} />;
+  // const [socket, setSocket] = useState();
+  // // Connect to the Socket.IO server
+  // useEffect(() => {
+  //   const socket = io("http://localhost:8080"); // Replace with your server URL
+  //   // Clean up on component unmount
+  //   setSocket(socket);
+  //   return () => {
+  //     socket.disconnect();
+  //   };
+  // }, []);
+  // // return <Landing socket={socket} />;
+
+  return <Home />;
 };
 
 export default App;
---
+++ flowrtc/src/pages/home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-neutral-900 ">
+      <div className="container min-h-screen min-w-full">
+        <div className="text-white font-doto">FlowRTC</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
