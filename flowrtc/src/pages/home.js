6930382acba4897f2fef1b76be939e2cb19df002
Pritish <pritish@Pritishs-MacBook-Air.local>
2024-12-06 17:42:14 +0530

    updated the prettier config

--- flowrtc/src/pages/home.js
+++ flowrtc/src/pages/home.js
@@ -1,8 +1,8 @@
 const Home = () => {
   return (
-    <div className="min-h-screen bg-neutral-900 ">
+    <div className="min-h-screen bg-neutral-900">
       <div className="container min-h-screen min-w-full">
-        <div className="text-white font-doto">FlowRTC</div>
+        <div className="font-doto text-white">FlowRTC</div>
       </div>
     </div>
   );
